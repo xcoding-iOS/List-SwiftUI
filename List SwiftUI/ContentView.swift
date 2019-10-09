ddffea1b1cc294ba27ed4a62244f384b7837eec3
Giuseppe <giuseppe.sapienza@xcoding.it>
2019-10-09 18:55:08 +0200

    completed

--- List SwiftUI/ContentView.swift
+++ List SwiftUI/ContentView.swift
@@ -9,8 +9,59 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    var books: [Book] = [
+         Book.init(title: "Clean Code",
+                         link: "https://amzn.to/2YqSuEu",
+                         image: #imageLiteral(resourceName: "cleancode")),
+               Book.init(title: "Elegant Objects",
+                         link: "https://amzn.to/32N4dg0",
+                         image: #imageLiteral(resourceName: "elegant")),
+               Book.init(title: "Design Patterns",
+                         link: "https://amzn.to/2YkHCZ2",
+                         image: #imageLiteral(resourceName: "pattern")),
+    ]
+    
+    var contacts: [String] = ["Giuseppe", "Luca", "Matteo", "Edoardo", "Seby", "Gabriele", "Dario"]
+    
     var body: some View {
-        Text("Hello World")
+        VStack(alignment: .leading) {
+            Text("Programming Books")
+                .font(.largeTitle)
+                .padding()
+            
+            List(
+                books,
+                id: \.title,
+                rowContent: { book in
+                    
+                    Image(uiImage: book.image)
+                        .resizable()
+                        .aspectRatio(contentMode: .fill)
+                        .frame(width: 120, height: 160)
+                        .cornerRadius(8)
+                        .shadow(radius: 20)
+                        .padding([.top, .bottom])
+                    
+                    
+                    VStack(alignment: .leading) {
+                        Text(book.title)
+                            .fontWeight(.bold)
+                        Text(book.link)
+                            .font(.callout)
+                    }
+                }
+            )
+            
+            /*
+            List(
+                contacts,
+                id: \.hash,
+                rowContent:
+            { contact in
+                Text(contact)
+            }) */
+        }
     }
 }
 
---
+++ List SwiftUI/Model/Book.swift
@@ -0,0 +1,16 @@
+//
+//  Book.swift
+//  List e ForEach
+//
+//  Created by Giuseppe Sapienza on 07/10/2019.
+//  Copyright © 2019 Giuseppe Sapienza. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+struct Book {
+    let title: String
+    let link: String
+    let image: UIImage
+}
