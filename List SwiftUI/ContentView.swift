26aa69d9761f89d250b6ce04e45dd00d66c8caf2
Giuseppe <Sapienza>
2020-05-05 21:22:17 +1000

    navigation completed

--- List SwiftUI/ContentView.swift
+++ List SwiftUI/ContentView.swift
@@ -11,45 +11,67 @@ import SwiftUI
 struct ContentView: View {
     
     var books: [Book] = [
-         Book.init(title: "Clean Code",
-                         link: "https://amzn.to/2YqSuEu",
-                         image: #imageLiteral(resourceName: "cleancode")),
-               Book.init(title: "Elegant Objects",
-                         link: "https://amzn.to/32N4dg0",
-                         image: #imageLiteral(resourceName: "elegant")),
-               Book.init(title: "Design Patterns",
-                         link: "https://amzn.to/2YkHCZ2",
-                         image: #imageLiteral(resourceName: "pattern")),
+        Book(title: "Clean Code",
+             link: "https://amzn.to/2YqSuEu",
+             image: #imageLiteral(resourceName: "cleancode")),
+        
+        Book(title: "A Philosophy of Software Design",
+             link: "https://amzn.to/2xxhkaw",
+             image: #imageLiteral(resourceName: "philosophy")),
+        
+        Book(title: "Design Patterns",
+             link: "https://amzn.to/2YkHCZ2",
+             image: #imageLiteral(resourceName: "pattern"))
     ]
     
+    @State var searchIsPresented: Bool = false
+    
     var body: some View {
-        VStack(alignment: .leading) {
-            Text("Programming Books")
-                .font(.largeTitle)
-                .padding()
-            
-            List(
-                books,
-                id: \.title,
-                rowContent: { book in
-                    
-                    Image(uiImage: book.image)
-                        .resizable()
-                        .aspectRatio(contentMode: .fill)
-                        .frame(width: 120, height: 160)
-                        .cornerRadius(8)
-                        .shadow(radius: 20)
-                        .padding([.top, .bottom])
-                    
-                    
-                    VStack(alignment: .leading) {
-                        Text(book.title)
-                            .fontWeight(.bold)
-                        Text(book.link)
-                            .font(.callout)
-                    }
+        NavigationView {
+            VStack(alignment: .leading) {
+                
+                List(
+                    books,
+                    id: \.title,
+                    rowContent: { book in
+                        
+                        NavigationLink(destination: Text(book.title), label: {
+                            Image(uiImage: book.image)
+                                .resizable()
+                                .aspectRatio(contentMode: .fill)
+                                .frame(width: 120, height: 160)
+                                .cornerRadius(8)
+                                .shadow(radius: 20)
+                                .padding([.top, .bottom])
+                            
+                            VStack(alignment: .leading) {
+                                Text(book.title)
+                                    .fontWeight(.bold)
+                                Text(book.link)
+                                    .font(.callout)
+                            }
+                        })
+                        
                 })
+                .navigationBarTitle("Programming Books")
+                .navigationBarItems(trailing: Button(action: {
+                    self.searchIsPresented.toggle()
+                }, label: { Text("Search") }))
+            }
         }
+        .sheet(isPresented: $searchIsPresented, content: {
+            VStack {
+                Text("Search View")
+                
+                Spacer()
+                
+                Button(action: {
+                    self.searchIsPresented.toggle()
+                }, label: {
+                    Text("Close")
+                })
+            }.padding()
+        })
     }
 }
 
